4ae798251e2d0e6006756abc5dda4dd3b902fbd6
4mbk0r <4mbk0r@gmail.com>
2018-03-23 13:49:35 -0400

    guarfar

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+function saludar(nomb) {
+    console.log("hola" + nomb);
+}
+var wolverin = {
+    nombre: "login"
+};
+saludar(wolverin.nombre);
---
+++ app.ts
@@ -0,0 +1,9 @@
+
+function saludar(nomb:string) {
+	console.log("hola"+nomb)
+}
+var wolverin = {
+	nombre:"login"
+}
+
+saludar ( wolverin.nombre );
